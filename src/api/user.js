42fd0989caae9e5b3109957fb5c0a0dc5453f8e8
Ligg <156616301+liggmxa2003@users.noreply.github.com>
2024-12-12 18:03:39 +0800

    完成注册操作

--- src/api/user.js
+++ src/api/user.js
@@ -1,3 +1,12 @@
 //导入request.js请求工具
 import request from "@/utils/request";
 
+//注册接口
+export const userRegisterService = (registerData) => {
+    //借助于UrlSearchParams将数据转换为url参数，完成传递
+    const params = new URLSearchParams()
+    for (let key in registerData) {
+        params.append(key, registerData[key]);
+    }
+    return request.post('/user/register', params);
+}
--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,20 @@ import path from 'path'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: 5173
+    port: 5173,
+    proxy: {
+        '/api': { //获取路径中包含api的请求
+          target: 'http://localhost:8080', //后台服务所在的源
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '') // 将路径中的api替换为空
+        }
+      }
   },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
-    }
+    },
+    
   }
+
 }) 
